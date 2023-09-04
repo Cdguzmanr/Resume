a73b330b2313484e5fddb2d7e4bf7c4538764721
Carlos Guzmán Ramírez <cguzmanr17@gmail.com>
2023-09-03 22:28:56 -0500

    Add project files.

---
+++ CG.Resume.UI/Pages/Work.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CG.Resume.UI.Pages
+{
+    public class WorkModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
